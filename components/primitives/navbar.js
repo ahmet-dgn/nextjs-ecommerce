a9dcb38bedbc84dc7a2955b520817ca491e9a359
Ahmet Can Doğan <ahmetcandgn@gmail.com>
2024-06-27 09:58:29 +0300

    Design bug fixed

--- components/primitives/navbar.js
+++ components/primitives/navbar.js
@@ -176,13 +176,13 @@ export default function Navbar({ navigation }) {
               <div className="flex items-center space-x-6">
                 <Link
                   href="#"
-                  className="text-sm font-medium text-gray-900 hover:text-gray-700"
+                  className="text-sm font-medium text-gray-900 hover:text-gray-500"
                 >
                   Giriş yap
                 </Link>
                 <Link
                   href="#"
-                  className="text-sm font-medium text-gray-900 hover:text-gray-700"
+                  className="text-sm font-medium text-gray-900 hover:text-gray-500"
                 >
                   Kayıt ol
                 </Link>
